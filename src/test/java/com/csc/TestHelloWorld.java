fb8e1ff54523886ddcb996824c40bb1f1f77ad5a
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2024-12-30 12:01:44 -0600

    Update the build.gradle file to run the program

--- src/test/java/com/csc/TestHelloWorld.java
+++ src/test/java/com/csc/TestHelloWorld.java
@@ -2,11 +2,7 @@ package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import java.io.*;
 
 public class TestHelloWorld {
